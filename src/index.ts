840625fe0496aebd497bb6ed570114343ca86476
Angel Hernandez <angel.hernandez.12@live.com>
2023-03-03 13:20:29 -0400

    Fixed root path

--- src/index.ts
+++ src/index.ts
@@ -10,9 +10,6 @@ app.use(express.json());
 app.use(express.raw({ type: "application/vnd.custom-type" }));
 app.use(express.text({ type: "text/html" }));
 
-app.get("/", async (req, res) => {
-  res.json({ message: "Please visit /countries to view all the countries" });
-});
 
 app.use("/countries", countryRoutes);
 
