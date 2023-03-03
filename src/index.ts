9aaa5a9a90632f509924cc6f7a9e3b54f627c479
Angel Hernandez <angel.hernandez.12@live.com>
2023-03-03 13:18:46 -0400

    First Commit

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import countryRoutes from "./routes/country";
 const app = express();
 const port = process.env.PORT || 3333;
 
+app.use(express.static('public'))
 app.use(express.json());
 app.use(express.raw({ type: "application/vnd.custom-type" }));
 app.use(express.text({ type: "text/html" }));
---
+++ vue-project/src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <h1>My Vue 3, TypeScript, Pinia, Express, MongoDB, Mongoose App</h1>
+  </main>
+</template>
