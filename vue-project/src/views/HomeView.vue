360e47a35f6432b2c28cc0175a4ed7a52677d3dc
Angel Hernandez <angel.hernandez.12@live.com>
2023-03-03 13:32:16 -0400

    Delete vue-project directory

--- vue-project/src/views/HomeView.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
-<template>
-  <main>
-    <h1>My Vue 3, TypeScript, Pinia, Express, MongoDB, Mongoose App</h1>
-  </main>
-</template>
