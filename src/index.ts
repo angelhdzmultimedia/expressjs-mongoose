2599a41576709e90e67af0fd17f399d51b6039b1
Angel Hernandez <angel.hernandez.12@live.com>
2023-03-03 13:39:32 -0400

    asdasdqwe

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import "./lib/db";
 import express from "express";
 import countryRoutes from "./routes/country";
+import { join, resolve } from 'node:path'
 
 const app = express();
 const port = process.env.PORT || 3333;
 
-app.use(express.static('public'))
+app.use(express.static(resolve(join(process.cwd(), 'public'))))
 app.use(express.json());
 app.use(express.raw({ type: "application/vnd.custom-type" }));
 app.use(express.text({ type: "text/html" }));
